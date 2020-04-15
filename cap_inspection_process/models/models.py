b75db42ce2e0f5bbebe41b55fef10ec1102ebcdd
captivea-jbb <56834381+captivea-jbb@users.noreply.github.com>
2020-04-15 18:05:54 -0400

    Update models.py

--- cap_inspection_process/models/models.py
+++ cap_inspection_process/models/models.py
@@ -12,7 +12,8 @@ class Transfer(models.Model):
         res = self.read()
         res = {'move_ids_without_package': self.move_ids_without_package}
         datas = {
-            'ids': [self.id],
+            # 'ids': [self.id],
+            'ids': self.env.context.get('active_ids', []),
             'model': 'stock.picking',
             'form': res,
             'docs': [self.id]
