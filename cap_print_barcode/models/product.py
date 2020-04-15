52170368317ee461b00bf5d7944e95ee0cae7f7f
captivea-jbb <56834381+captivea-jbb@users.noreply.github.com>
2020-04-15 13:40:40 -0400

    Update product.py

--- cap_print_barcode/models/product.py
+++ cap_print_barcode/models/product.py
@@ -12,15 +12,15 @@ class Product(models.Model):
 
    
     def print_report_barcode(self):
-    #   return self.env.ref('product.report_productbarcode').report_action(self)
-      return {
-            'type': 'ir.actions.report.xml', 
-            'report_name':'product.report_productbarcode',
-            'datas': {
-                    'model':'account.voucher',
-                    'id': self.id,
-                    'ids': [self.id],
-                    'report_type': 'webkit'
-                },
-            'nodestroy': True
-            }
+      return self.env.ref('product.report_productbarcode').report_action(self)
+    #   return {
+    #         'type': 'ir.actions.report.xml', 
+    #         'report_name':'product.report_productbarcode',
+    #         'datas': {
+    #                 'model':'account.voucher',
+    #                 'id': self.id,
+    #                 'ids': [self.id],
+    #                 'report_type': 'webkit'
+    #             },
+    #         'nodestroy': True
+    #         }
