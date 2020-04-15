cdaf50d706a346553caec6886854b826b5993b69
captivea-jbb <56834381+captivea-jbb@users.noreply.github.com>
2020-04-15 16:16:33 -0400

    Update models.py

--- cap_inspection_process/models/models.py
+++ cap_inspection_process/models/models.py
@@ -10,4 +10,4 @@ class Transfer(models.Model):
         datas = { 'ids': [self.id],}
         # return self.env['ir.actions.report'].search([('id','=',706)])[0].report_action([self.id], data=datas)
         # return self.env.ref('studio_customization.studio_report_docume_a8b80cba-44d2-4d4c-a834-a5e9e072854e').report_action([self.id], data=datas)
-        return {'type': 'ir.actions.report','report_name': 'studio_customization.studio_report_docume_a8b80cba-44d2-4d4c-a834-a5e9e072854','report_type':"qweb-pdf",'data': datas,}
+        return {'type': 'ir.actions.report','report_name': 'studio_customization.studio_report_docume_a8b80cba-44d2-4d4c-a834-a5e9e072854','report_type':"qweb-pdf",'data': context,}
