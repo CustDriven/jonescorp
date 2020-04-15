385a85a4a74a263540b4eac672b66e6005eb03b8
captivea-jbb <56834381+captivea-jbb@users.noreply.github.com>
2020-04-15 17:08:48 -0400

    Update models.py

--- cap_inspection_process/models/models.py
+++ cap_inspection_process/models/models.py
@@ -10,15 +10,22 @@ class Transfer(models.Model):
         # datas = { 'ids': [self.id],}
         # return self.env['ir.actions.report'].search([('id','=',706)])[0].report_action([self.id], data=datas)
         # return self.env.ref('studio_customization.studio_report_docume_a8b80cba-44d2-4d4c-a834-a5e9e072854e').report_action([self.id], data=datas)
-        context = self._context
-        [data] = self.read()
+        # context = self._context
+        # [data] = self.read()
         # data['emp'] = self.env.context.get('active_ids', [])
         # employees = self.env['stock.picking'].browse(data['emp'])
-        res = {'move_ids_without_package': self.move_ids_without_package}
-        datas = {
-            'ids': [self.id],
-            'model': 'stock.picking',
-            'form': res
-            }
+        # res = {'move_ids_without_package': self.move_ids_without_package}
+        # datas = {
+        #     'ids': [self.id],
+        #     'model': 'stock.picking',
+        #     'form': res
+        #     }
 
-        return {'type': 'ir.actions.report','report_name': 'studio_customization.studio_report_docume_a8b80cba-44d2-4d4c-a834-a5e9e072854','report_type':"qweb-pdf",'data': datas}
+        # return {'type': 'ir.actions.report','report_name': 'studio_customization.studio_report_docume_a8b80cba-44d2-4d4c-a834-a5e9e072854','report_type':"qweb-pdf",'data': datas}
+
+        datas = {'ids': self.env.context.get('active_ids', [])}
+        res = self.read(['move_ids_without_package', 'id'])
+        res = res and res[0] or {}
+        res['move_ids_without_package'] = res['move_ids_without_package']
+        datas['form'] = res
+        return self.env.ref('studio_customization.studio_report_docume_a8b80cba-44d2-4d4c-a834-a5e9e072854').report_action([], data=datas)
