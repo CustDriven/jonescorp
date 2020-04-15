9cfb4f51b21df91d2243c46f4de57b3433891142
captivea-jbb <56834381+captivea-jbb@users.noreply.github.com>
2020-04-15 16:05:33 -0400

    Update models.py

--- cap_inspection_process/models/models.py
+++ cap_inspection_process/models/models.py
@@ -8,4 +8,5 @@ class Transfer(models.Model):
 
     def print_barcode(self):
         datas = { 'ids': [self.id],}
-        return self.env['ir.actions.report'].search([('id','=',706)])[0].report_action([self.id], data=datas)
+        # return self.env['ir.actions.report'].search([('id','=',706)])[0].report_action([self.id], data=datas)
+        return self.env.ref('studio_customization.studio_report_docume_a8b80cba-44d2-4d4c-a834-a5e9e072854e	').report_action([self.id], data=datas)
