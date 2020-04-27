8657fa577cb968e97a991dcd8e24b5e4dbb386bc
captivea-jbb <56834381+captivea-jbb@users.noreply.github.com>
2020-04-27 10:56:25 -0400

    Update models.py

--- cap_inspection_process/models/models.py
+++ cap_inspection_process/models/models.py
@@ -7,35 +7,4 @@ class Transfer(models.Model):
     _inherit = 'stock.picking'                                  
 
     def print_barcode(self):
-        # datas = { 'ids': [self.id],}
-
-        # res = self.read()
-        # res = {'move_ids_without_package': self.move_ids_without_package}
-        # datas = {
-        #     'ids': [self.id, 67],
-        #     # 'ids': self.env.context.get('active_ids', []),
-        #     'model': 'stock.picking',
-        #     'form': res,
-        #     'doc_ids': [self.id]
-        #     }
-        # return self.env['ir.actions.report'].search([('id','=',706)])[0].report_action([self.id], data=datas)
         return self.env.ref('studio_customization.transfer_report_5dadae11-98a4-4a49-b45f-a14e1dc2d03b').report_action(self)
-        # context = self._context
-        # [data] = self.read()
-        # data['emp'] = self.env.context.get('active_ids', [])
-        # employees = self.env['stock.picking'].browse(data['emp'])
-        # res = {'move_ids_without_package': self.move_ids_without_package}
-        # datas = {
-        #     'ids': [self.id],
-        #     'model': 'stock.picking',
-        #     'form': res
-        #     }
-
-        # return {'type': 'ir.actions.report','report_name': 'studio_customization.studio_report_docume_a8b80cba-44d2-4d4c-a834-a5e9e072854','report_type':"qweb-pdf",'data': datas}
-
-        # datas = {'ids': self.env.context.get('active_ids', [])}
-        # res = self.read(['move_ids_without_package', 'id'])
-        # res = res and res[0] or {}
-        # res['move_ids_without_package'] = res['move_ids_without_package']
-        # datas['form'] = res
-        # return self.env.ref('studio_customization.transfer_report_5dadae11-98a4-4a49-b45f-a14e1dc2d03b').report_action([], data=datas)
