c64618c9224a6527a7e31cd59346f57a38d32302
captivea-jbb <56834381+captivea-jbb@users.noreply.github.com>
2020-09-18 13:16:25 -0400

    Update models.py

--- cap_inspection_process/models/models.py
+++ cap_inspection_process/models/models.py
@@ -16,43 +16,46 @@ class Transfer(models.Model):
     def print_picking(self):
         return self.env.ref('stock.action_report_picking').report_action(self)
 
-# class Lead2OpportunityPartner(models.TransientModel):
+class Lead2OpportunityPartner(models.TransientModel):
 
-#     _inherit = 'crm.lead2opportunity.partner'
+    _inherit = 'crm.lead2opportunity.partner'
 
-#     def action_apply(self):
-#         """ Convert lead to opportunity or merge lead and opportunity and open
-#             the freshly created opportunity view.
-#         """
-#         self.ensure_one()
-#         values = {
-#             'team_id': self.team_id.id,
-#         }
+    def action_apply(self):
+        """ Convert lead to opportunity or merge lead and opportunity and open
+            the freshly created opportunity view.
+        """
 
-#         if self.partner_id:
-#             values['partner_id'] = self.partner_id.id
+        _logger.info("LAAAAAAAAAAAUNCHED !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
 
-#         if self.name == 'merge':
-#             leads = self.with_context(active_test=False).opportunity_ids.merge_opportunity()
-#             if not leads.active:
-#                 leads.write({
-#                     'active': True,
-#                     'activity_type_id': False,
-#                     'lost_reason': False,   
-#                     'date_deadline': self.x_estimated_close_date,
-#                     'planned_revenue': self.x_estimated_value,
-#                     'x_opportunity_type': self.x_opp_type,
-#                     'stage_id': 1,
-#                 })
-#             if leads.type == "lead":
-#                 values.update({'lead_ids': leads.ids, 'user_ids': [self.user_id.id]})
-#                 self.with_context(active_ids=leads.ids)._convert_opportunity(values)
-#             elif not self._context.get('no_force_assignation') or not leads.user_id:
-#                 values['user_id'] = self.user_id.id
-#                 leads.write(values)
-#         else:
-#             leads = self.env['crm.lead'].browse(self._context.get('active_ids', []))
-#             values.update({'lead_ids': leads.ids, 'user_ids': [self.user_id.id]})
-#             self._convert_opportunity(values)
+        self.ensure_one()
+        values = {
+            'team_id': self.team_id.id,
+        }
 
-#         return leads[0].redirect_lead_opportunity_view()
+        if self.partner_id:
+            values['partner_id'] = self.partner_id.id
+
+        if self.name == 'merge':
+            leads = self.with_context(active_test=False).opportunity_ids.merge_opportunity()
+            if not leads.active:
+                leads.write({
+                    'active': True,
+                    'activity_type_id': False,
+                    'lost_reason': False,   
+                    'date_deadline': self.x_estimated_close_date,
+                    'planned_revenue': self.x_estimated_value,
+                    'x_opportunity_type': self.x_opp_type,
+                    'stage_id': 1,
+                })
+            if leads.type == "lead":
+                values.update({'lead_ids': leads.ids, 'user_ids': [self.user_id.id]})
+                self.with_context(active_ids=leads.ids)._convert_opportunity(values)
+            elif not self._context.get('no_force_assignation') or not leads.user_id:
+                values['user_id'] = self.user_id.id
+                leads.write(values)
+        else:
+            leads = self.env['crm.lead'].browse(self._context.get('active_ids', []))
+            values.update({'lead_ids': leads.ids, 'user_ids': [self.user_id.id]})
+            self._convert_opportunity(values)
+
+        return leads[0].redirect_lead_opportunity_view()
