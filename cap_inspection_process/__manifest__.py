e7d47c7b4916e14d2f0d939e6c33e15f860333be
captivea-jbb <56834381+captivea-jbb@users.noreply.github.com>
2020-10-07 13:21:25 -0400

    Update __manifest__.py

--- cap_inspection_process/__manifest__.py
+++ cap_inspection_process/__manifest__.py
@@ -20,7 +20,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'sale',  'project', 'stock', 'crm'],
+    'depends': ['base', 'sale',  'project', 'stock', 'crm', 'inventory'],
 
     # 'data': [
     #  # 'security/ir.model.access.csv',
